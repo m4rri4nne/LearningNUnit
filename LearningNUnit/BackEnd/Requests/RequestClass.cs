2dd453b83ad62d94984bd67d154675a8ed14a9f3
Alicia Marianne <aliciadepaulaeng@gmail.com>
2023-09-05 17:47:03 +0100

    Updating RequestClass.cs

--- LearningNUnit/BackEnd/Requests/RequestClass.cs
+++ LearningNUnit/BackEnd/Requests/RequestClass.cs
@@ -1,19 +1,31 @@
-using LearningNUnit.BackEnd.Entities;
 using RestSharp;
 
 namespace LearningNUnit.BackEnd.Requests;
 
 public class RequestClass
 {
+
     static string token = "AZZG1IIlYSKdTvQpaHeuKYqM30t0bsEZdbi7NtVZ";
     static string baseUrl = "https://api.nasa.gov";
 
-    public RestResponse NasaApiRequest()
+    public RestResponse NasaApiRequest(string? queryParameters=null)
     {
         RestClient client = new RestClient(baseUrl);
-        RestRequest restRequest= new RestRequest($"/planetary/apod?api_key={token}", Method.Get);
-        RestResponse restResponse= client.Execute(restRequest);
+        RestRequest restRequest = new RestRequest(NasaResource(token, queryParameters), Method.Get);
+        RestResponse restResponse = client.Execute(restRequest);
 
         return restResponse;
+    }
+
+    private string NasaResource(string token, string? queryParameters = null)
+    {   
+        var baseResource = $"/planetary/apod?api_key={token}";
+        if (queryParameters != null)
+        {
+            return baseResource + queryParameters;
+        }
+
+        return baseResource;
+
     }
 }
